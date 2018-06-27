8984ae150fe92a6f7ab76fa76c4a9b2eb5605bae
Adam Pearce <1wheel@gmail.com>
2018-06-27 02:02:49 -0400

    adds update

--- scrape/riot/parse-games.js
+++ scrape/riot/parse-games.js
@@ -2,21 +2,19 @@ var { _, d3, fs, glob, io, queue, request } = require('scrape-stl')
 
 
 var matches = glob.sync(__dirname + '/raw/matchtimeline/*').forEach((path, i) => {
+  console.log(path)
   var game = path.split('matchtimeline/')[1].replace('.json', '')
   
   var outFrames = []
   var outEvents = []
   io.readDataSync(path).frames.forEach(d => {
     var timestamp = d.timestamp
-
+    // console.log(timestamp)
     d3.values(d.participantFrames).forEach(d => {
       d.game = game
       d.timestamp = timestamp
 
-      var {x, y} = d.position
-      d.x = x
-      d.y = y
-      delete d.position
+      if (d.position) flattenPosition(d)
 
       outFrames.push(d)
     })
@@ -24,18 +22,20 @@ var matches = glob.sync(__dirname + '/raw/matchtimeline/*').forEach((path, i) =>
     d.events.forEach(d => {
       d.game = game
 
-      if (d.position){
-        var {x, y} = d.position
-        d.x = x
-        d.y = y
-        delete d.position
-      }
+      if (d.position) flattenPosition(d)
 
       if (d.assistingParticipantIds) d.assistingParticipantIds = d.assistingParticipantIds.join(' ')
 
       outEvents.push(d)
     })
   })
+
+  function flattenPosition(d){
+    var {x, y} = d.position
+    d.x = x
+    d.y = y
+    delete d.position
+  }
 
 
   var outPath = path
---
+++ scrape/riot/update.sh
@@ -0,0 +1,4 @@
+node download-leagues.js && \
+node download-highlanderMatches.js &&
+node download-games.js &&
+node parse-games.js
